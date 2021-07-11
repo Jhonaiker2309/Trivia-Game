c74c546511a174d2fa2f46f178bda43eaa374325
Jhonaiker <blancojhonaiker@gmail.com>
2021-07-11 03:57:47 -0500

    best

--- back-end/index.js
+++ back-end/index.js
@@ -1,65 +1,73 @@
-const express = require("express")
+const express = require( "express" )
 const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
+const cors = require( "cors" )
+const mongoose = require( "mongoose" )
 const Schema = mongoose.Schema
-require("dotenv").config()
+require( "dotenv" ).config()
 
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use( cors() )
+app.use( express.json() )
+app.use( express.urlencoded( {
+  extended: true
+} ) )
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}
-);
+mongoose.connect( process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+} );
 
-const resultSchema ={
+const resultSchema = {
   name: String,
   time: Number,
   score: Number
 }
 
-const resultadoTotal = new Schema(resultSchema)
+const resultadoTotal = new Schema( resultSchema )
 
 
-const Result = mongoose.model("Result", resultadoTotal)
+const Result = mongoose.model( "Result", resultadoTotal )
 
-app.get("/result", (req,res) => {
-    Result.find()
-    .then((results)=> res.json(results))
-    .catch((err) => res.status(400).json("Error: " +err))
-})
+app.get( "/result", async ( req, res ) => {
+  await Result.find()
+    .then( ( results ) => res.json( results ) )
+    .catch( ( err ) => res.status( 400 ).json( "Error: " + err ) )
+} )
 
-app.post("/result", (req,res) => {
-    const newResult = new Result({
-        name: req.body.name,
-        time: req.body.time,
-        score: req.body.score
-    })
+app.post( "/result", async ( req, res ) => {
+  const newResult = new Result( {
+    name: req.body.name,
+    time: req.body.time,
+    score: req.body.score
+  } )
 
-    newResult.save()
-    .then(result => console.log(result))
-    .catch(err => res.status(400).json("Error" + err))
-    
-})
+  await newResult.save()
+    .then( result => console.log( result ) )
+    .catch( err => res.status( 400 ).json( "Error" + err ) )
 
-app.delete("/result/:id", (req, res) => {
+} )
+
+app.delete( "/result/:id", async ( req, res ) => {
   const id = req.params.id;
 
-  Result.findByIdAndDelete({ _id: id }, (req, res, err) => {
-    if (!err) {
-      console.log("Item deleted");
-    } else {
-      console.log(err);
-    }
-  });
-});
+  await Result.findByIdAndDelete
+    .then( {
+      _id: id
+    }, ( req, res, err ) => {
+      if ( !err ) {
+        console.log( "Item deleted" );
+      } else {
+        console.log( err );
+      }
+    } )
+    .catch( err => console.log( err ) );
+} );
 
-    app.use(express.static('client/build'));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+app.use( express.static( 'client/build' ) );
+app.get( "*", ( req, res ) => {
+  res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ) );
+} )
 
-app.listen(process.env.PORT || 5000, function(){
-    console.log("Server running")
-})
+app.listen( process.env.PORT || 5000, function () {
+  console.log( "Server running" )
+} )
